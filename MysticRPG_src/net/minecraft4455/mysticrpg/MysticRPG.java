39b957e5add12d664fd5fe114ce8ad7a5dad5faa
PizzAna <pizzananpelailut@gmail.com>
2013-09-25 09:14:35 +0300

    More testing with version check

--- MysticRPG_src/net/minecraft4455/mysticrpg/MysticRPG.java
+++ MysticRPG_src/net/minecraft4455/mysticrpg/MysticRPG.java
@@ -12,6 +12,7 @@ import net.minecraft4455.mysticrpg.core.proxys.CommonProxy;
 import net.minecraft4455.mysticrpg.core.recipes.MysticRecipes;
 import net.minecraftforge.common.Configuration;
 import pizzana.lib.common.VersionCheck;
+import pizzana.lib.sided.ClientTickHandler;
 import cpw.mods.fml.common.FMLLog;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.Mod.EventHandler;
@@ -19,6 +20,8 @@ import cpw.mods.fml.common.Mod.Instance;
 import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import cpw.mods.fml.common.network.NetworkMod;
+import cpw.mods.fml.common.registry.TickRegistry;
+import cpw.mods.fml.relauncher.Side;
 
 @Mod(name = MysticModInfo.NAME, modid = MysticModInfo.MODID, version = MysticModInfo.VERSION)
 @NetworkMod(clientSideRequired = MysticModInfo.CLIENT, serverSideRequired = MysticModInfo.SERVER)
@@ -46,6 +49,8 @@ public class MysticRPG {
 
         VersionCheck
                 .updateCheck(MysticModInfo.NAME, MysticModInfo.VERSION, URL);
+        
+        TickRegistry.registerTickHandler(new ClientTickHandler(), Side.CLIENT);
 
         logger = Logger.getLogger(MysticModInfo.MODID);
         logger.setParent(FMLLog.getLogger());
